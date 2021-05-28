bc9ab204a68ff67a712bc57fbbc22f73d5ee87be
fanuelfeto <76951255+fanuelfeto@users.noreply.github.com>
2021-05-28 17:15:33 +0700

    create base

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
 
 class AdminController extends Controller
 {
--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::namespace('Admin')->prefix('admin')->name('admin.')->group
+Route::get('','AdminAuthController@index')->name('index');
+		
+
+
