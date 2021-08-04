8ec185d54c1c78620b79c4f1b86725b77465b895
DarkGameMaster <e.dark.gm@gmail.com>
2021-08-04 22:48:02 +0700

    Fixed Delete Item Feature for Cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,89 +3,86 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
 
 use App\CollectionItem;
 
 class CartController extends Controller
 {
 
-public function addToCart(Request $request)
-{
+	public function addToCart(Request $request)
+	{
 
-    $this->validate($request, [
-        'product_id' => 'required',
-    ]);
+		$this->validate($request, [
+			'product_id' => 'required',
+		]);
 
-    $carts = json_decode($request->cookie('dw-carts'), true); 
-  
-    $product = CollectionItem::find($request->product_id);
-    
-    $carts[$request->product_id] = [
-            'qty' => 1,
-            'product_id' => $product->id,
-            'product_name' => $product->name,
-            'product_price' => $product->price,
-            'product_image' => $product->picture1,
-            'collection_id' => $product->collection_id
-    ];
+		$carts = json_decode($request->cookie('dw-carts'), true); 
 
-    $cookie = cookie('dw-carts', json_encode($carts), 60);
- 
-    return redirect()->back()->cookie($cookie);
+		$product = CollectionItem::find($request->product_id);
+
+		$carts[$request->product_id] = [
+			'qty' => 1,
+			'product_id' => $product->id,
+			'product_name' => $product->name,
+			'product_price' => $product->price,
+			'product_image' => $product->picture1,
+			'collection_id' => $product->collection_id
+		];
+
+		$cookie = cookie('dw-carts', json_encode($carts), 60);
+
+		return redirect()->back()->cookie($cookie);
+	}
+
+	public function listCart()
+	{
+
+		$carts = json_decode(request()->cookie('dw-carts'), true);
+
+		$subtotal = collect($carts)->sum(function($q) {
+			return $q['product_price']; 
+		});
+
+		return view('user.cart', compact('carts', 'subtotal'));
+	}
+
+	public function updateCart(Request $request)
+	{
+		$carts = json_decode(request()->cookie('dw-carts'), true);
+		foreach($request->product_id as $key => $row) {
+			if($request->qty[$key] == 0) {
+				unset($carts[$row]);
+			} else {
+				$carts[$row]['qty'] = $request->qty[$key];
+			}
+		}
+		$cookie = cookie('dw-carts', json_encode($carts), 60);
+		return redirect()->back()->cookie($cookie);
+	}
+
+	public function deleteCart(Request $request)
+	{
+		$prod_id = $request->input('product_id');
+
+		$cookie_data = stripslashes(Cookie::get('dw-carts'));
+		$cart_data = json_decode($cookie_data, true);
+
+		$item_id_list = array_column($cart_data, 'product_id');
+
+		if(in_array($prod_id, $item_id_list))
+		{
+			foreach($cart_data as $keys => $values)
+			{
+				if($cart_data[$keys]["product_id"] == $prod_id)
+				{
+					unset($cart_data[$keys]);
+					$item_data = json_encode($cart_data);
+					$minutes = 60;
+					Cookie::queue(Cookie::make('dw-carts', $item_data, $minutes));
+					return response()->json(['status'=>'Item Removed from Cart']);
+				}
+			}
+		}
+	}
 }
-
-public function listCart()
-{
-
-    $carts = json_decode(request()->cookie('dw-carts'), true);
-
-    $subtotal = collect($carts)->sum(function($q) {
-        return $q['product_price']; 
-    });
-
-    return view('user.cart', compact('carts', 'subtotal'));
-    
-}
-
-public function updateCart(Request $request)
-    {
-        $carts = json_decode(request()->cookie('dw-carts'), true);
-        foreach ($request->product_id as $key => $row) {
-            if ($request->qty[$key] == 0) {
-                unset($carts[$row]);
-            } else {
-                $carts[$row]['qty'] = $request->qty[$key];
-            }
-        }
-        $cookie = cookie('dw-carts', json_encode($carts), 60);
-        return redirect()->back()->cookie($cookie);
-    }
-
-public function deleteCart(Request $request)
-    {
-        $prod_id = $request->input('product_id');
-
-        $cookie_data = stripslashes(Cookie::get('dw-carts'));
-        $cart_data = json_decode($cookie_data, true);
-
-        $item_id_list = array_column($cart_data, 'product_id');
-        $prod_id_is_there = $prod_id;
-
-        if(in_array($prod_id_is_there, $item_id_list))
-        {
-            foreach($cart_data as $keys => $values)
-            {
-                if($cart_data[$keys]["product_id"] == $prod_id)
-                {
-                    unset($cart_data[$keys]);
-                    $item_data = json_encode($cart_data);
-                    $minutes = 60;
-                    Cookie::queue(Cookie::make('dw-carts', $item_data, $minutes));
-                    return response()->json(['status'=>'Item Removed from Cart']);
-                }
-            }
-        }
-
-    }
-}
-
--- resources/views/user/cart.blade.php
+++ resources/views/user/cart.blade.php
@@ -1,24 +1,24 @@
 @extends('layouts.user.user')
 @section('title','Cart')
 @section('content')  
-   <!--================Home Banner Area =================-->
-	<section class="banner_area">
-		<div class="banner_inner d-flex align-items-center">
-			<div class="container">
-				<div class="banner_content text-center mt-20 mb-20">
-					<h3>Cart</h3>
-				</div>
+<!--================Home Banner Area =================-->
+<section class="banner_area">
+	<div class="banner_inner d-flex align-items-center">
+		<div class="container">
+			<div class="banner_content text-center mt-20 mb-20">
+				<h3>Cart</h3>
 			</div>
 		</div>
-	</section>
-	<!--================End Home Banner Area =================-->
+	</div>
+</section>
+<!--================End Home Banner Area =================-->
 
-	<!--================Cart Area =================-->
-	<section class="cart_area mb-80">
-		<div class="container">
-			<div class="cart_inner">
-                <form action="{{ route('updateCart') }}" method="post">
-                    @csrf
+<!--================Cart Area =================-->
+<section class="cart_area mb-80">
+	<div class="container">
+		<div class="cart_inner">
+			<form action="{{ route('updateCart') }}" method="post">
+				@csrf
 				<div class="table-responsive">
 					<table class="table">
 						<thead>
@@ -31,97 +31,97 @@
 							</tr>
 						</thead>
 						<tbody>
-                            @forelse ((array) $carts as $row)
+							@forelse ((array) $carts as $row)
 							<tr>
 								<td>
 									<div class="media">
 										@if ($row['collection_id'] == 1)
 										<div class="d-flex">
-                                            <img src="{{ asset('images/collections/home_accessories/' . $row['product_image']) }}" width="100px" height="100px" alt="{{ $row['product_name'] }}">
+											<img src="{{ asset('images/collections/home_accessories/' . $row['product_image']) }}" width="100px" height="100px" alt="{{ $row['product_name'] }}">
 										</div>
 										@elseif ($row['collection_id'] == 2)
 										<div class="d-flex">
-                                            <img src="{{ asset('images/collections/furniture/' . $row['product_image']) }}" width="100px" height="100px" alt="{{ $row['product_name'] }}">
+											<img src="{{ asset('images/collections/furniture/' . $row['product_image']) }}" width="100px" height="100px" alt="{{ $row['product_name'] }}">
 										</div>
 										@elseif ($row['collection_id'] == 3)
 										<div class="d-flex">
-                                            <img src="{{ asset('images/collections/paintings/' . $row['product_image']) }}" width="100px" height="100px" alt="{{ $row['product_name'] }}">
+											<img src="{{ asset('images/collections/paintings/' . $row['product_image']) }}" width="100px" height="100px" alt="{{ $row['product_name'] }}">
 										</div>
 										@elseif ($row['collection_id'] == 4)
 										<div class="d-flex">
-                                            <img src="{{ asset('images/collections/sculpture/' . $row['product_image']) }}" width="100px" height="100px" alt="{{ $row['product_name'] }}">
+											<img src="{{ asset('images/collections/sculpture/' . $row['product_image']) }}" width="100px" height="100px" alt="{{ $row['product_name'] }}">
 										</div>
 										@else
 										<div class="d-flex">
-                                            <img src="{{ asset('images/highlights/' . $row['product_image']) }}" width="100px" height="100px" alt="{{ $row['product_name'] }}">
+											<img src="{{ asset('images/highlights/' . $row['product_image']) }}" width="100px" height="100px" alt="{{ $row['product_name'] }}">
 										</div>
 										@endif
 									</div>
 								</td>
 								<td>
 									<div>
-                                            <p>{{ $row['product_name'] }}</p>
+										<p>{{ $row['product_name'] }}</p>
 									</div>
 								</td>
-								<input type="hidden" class="product_id" value="{{ $row['product_id'] }}" >
 								<td>
-                                    <h5>Rp {{ number_format($row['product_price']) }}</h5>
+									<h5>Rp {{ number_format($row['product_price']) }}</h5>
 								</td>
 								<td></td>
 								<td>
+									<input type="hidden" class="product_id" value="{{ $row['product_id'] }}" />
 									<a href="#" onclick="return confirm('Are sure want to cancel this order')" class="btn-danger delete_cart_data">Delete</a>
 								</td>
-                            </tr>
-                            @empty
-                            <tr>
-                                <td colspan="4">Tidak ada belanjaan</td>
-                            </tr>
-                            @endforelse
-                            </form>
-							<tr>
-								<td>
-									<h5>Subtotal</h5>
-								</td>
-								<td></td>
-								<td>
-                                    <h5>Rp {{ number_format($subtotal) }}</h5>
-								</td>
-								<td></td>
 							</tr>
-						</tbody>
-					</table>
-				</div>
+							@empty
+							<tr>
+								<td colspan="4">Tidak ada belanjaan</td>
+							</tr>
+							@endforelse
+						</form>
+						<tr>
+							<td>
+								<h5>Subtotal</h5>
+							</td>
+							<td></td>
+							<td>
+								<h5>Rp {{ number_format($subtotal) }}</h5>
+							</td>
+							<td></td>
+						</tr>
+					</tbody>
+				</table>
 			</div>
 		</div>
-	</section>
+	</div>
+</section>
 
 <script>
-	 $(document).ready(function () {
+	$(document).ready(function () {
 
-        $('.delete_cart_data').click(function (e) {
-            e.preventDefault();
+		$('.delete_cart_data').click(function (e) {
+			e.preventDefault();
 
-            var product_id = $(this).closest(".cartpage").find('.product_id').val();
+			var product_id = $(this).closest("td").find('.product_id').val();
 
-            var data = {
-                '_token': $('input[name=_token]').val(),
-                "product_id": product_id,
-            };
+			var data = {
+				'_token': $('input[name=_token]').val(),
+				"product_id": product_id,
+			};
 
-            // $(this).closest(".cartpage").remove();
 
-            $.ajax({
-                url: '/delete-from-cart',
-                type: 'DELETE',
-                data: data,
-                success: function (response) {
-                    window.location.reload();
-                }
-            });
-        });
+			$.ajax({
+				url: "{{ route('deleteCart') }}",
+				type: 'GET',
+				data: data,
+				success: function (response)
+				{
+					location.reload();
+				}
+			});
+		});
 
-    });
+	});
 
 </script>
-	<!--================End Cart Area =================-->
+<!--================End Cart Area =================-->
 @endsection
