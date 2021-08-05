e2fbb5f686e638a099f03924ae09a1bab73d9068
DarkGameMaster <e.dark.gm@gmail.com>
2021-08-05 09:56:14 +0700

    Update CountingService.php
    Small fixes

--- app/Services/CountingService.php
+++ app/Services/CountingService.php
@@ -9,7 +9,15 @@ class CountingService
 	public function itemsInCart()
 	{
 		$items_in_cookie = stripslashes(Cookie::get('dw-carts'));
-		$total_items = count(json_decode($items_in_cookie, true));
-		return $total_items;
+		if ($items_in_cookie)
+		{
+			if (isset($items_in_cookie))
+			{
+				$total_items = count(json_decode($items_in_cookie, true));
+				return $total_items;
+			}
+		}
+		
+		return 0;
 	}
 }
