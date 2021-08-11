6ea79e1ab4b0f13b46b0f43b62644b84a3d08e2b
DarkGameMaster <e.dark.gm@gmail.com>
2021-08-11 17:43:04 +0700

    Adjustment for Admin Auth Layout and Admin Login Form
    What's new?
    
    - Adjusted Admin Auth Layout
    - Adjusted Admin Login Form

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -2,52 +2,42 @@
 @section('title','Login')
 
 @section('content')
-<body class="c-app">
-  <div class="c-sidebar">
-    <!-- Sidebar content here -->
-  </div>
-  <div class="c-wrapper">
-    <header class="c-header">
-      <!-- Header content here -->
-    </header>
-    <div class="c-body">
-      <main class="c-main">
-        <div class="container">
-		  <div class="row">
-		    <div class="col-lg-8">
-		      <div class="align-items-center justify-content-center">
-		      	<img class="img-fluid" src="{{ asset('images/main-logo.jpeg') }}" />
-		      </div>
-		    </div>
-		    <div class="col-lg-4">
-		      <div class="col-12 col-sm-8 col-md-6 col-lg-12 px-xl-2 mx-auto">
-		      	 <h4 class="card-title mb-1">Welcome!</h4>
-		      	 <p class="card-text mb-2">Please sign in to your account</p>
-		      	 <form method="POST" action="{{ url('admin/post-login') }}">
-		      	 	{{ csrf_field() }}
-				  <div class="form-group row">
-				    <label for="email" class="col-sm-2 col-form-label">Email</label>
-				    <div class="col-sm-10">
-				      <input type="text" class="form-control" name="email" id="email" value="email@example.com">
-				    </div>
-				  </div>
-				  <div class="form-group row">
-				    <label for="password" class="col-sm-2 col-form-label">Password</label>
-				    <div class="col-sm-10">
-				      <input type="password" class="form-control" name="password" id="password">
-				    </div>
-				  </div>
-				  <button type="submit" class="btn btn-primary mt-2">Log in</button>
-				</form>
-		      </div>
-		    </div>
-		  </div>
+<div class="container">
+	<div class="row justify-content-center">
+		<div class="col-md-6">
+			<div class="card mx-4">
+				<div class="card-body p-4">
+					<div class="text-center">
+						<img src="{{ asset('images/main-logo.jpeg') }}" class="img-fluid mb-3" width="200">
+					</div>
+					
+					<h1>Admin Login</h1>
+					<p class="text-muted">Login your account</p>
+					
+					<form method="POST" action="{{ url('admin/post-login') }}">
+						@csrf
+						<div class="input-group mb-3">
+							<div class="input-group-prepend">
+								<span class="input-group-text">
+									<i class="c-icon cil-envelope-open"></i>
+								</span>
+							</div>
+							<input type="email" class="form-control form-control-lg" name="email" id="email" placeholder="Email" required />
+						</div>
+						<div class="input-group mb-3">
+							<div class="input-group-prepend">
+								<span class="input-group-text">
+									<i class="c-icon cil-lock-locked"></i>
+								</span>
+							</div>
+							<input type="password" class="form-control form-control-lg" name="password" id="password" placeholder="Password" required />
+						</div>
+
+						<button type="submit" class="btn btn-block btn-success btn-lg">Login</button>
+					</form>
+				</div>
+			</div>
 		</div>
-      </main>
-    </div>
-    <footer class="c-footer">
-      <!-- Footer content here -->
-    </footer>
-  </div>
-</body>
+	</div>
+</div>
 @endsection
--- resources/views/layouts/admin/admin_auth.blade.php
+++ resources/views/layouts/admin/admin_auth.blade.php
@@ -5,20 +5,19 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<!-- CoreUI CSS -->
-	<link rel="stylesheet" href="https://unpkg.com/@coreui/coreui/dist/css/coreui.min.css" crossorigin="anonymous">
+	<!-- Bootstrap icons -->
+	<link rel="stylesheet" href="https://unpkg.com/@coreui/icons@2.0.0-beta.3/css/all.min.css">
+
+	<!-- CoreUI CSS v4.0.1 -->
+	<!-- <link rel="stylesheet" href="https://unpkg.com/@coreui/coreui@4.0.1/dist/css/coreui.min.css" crossorigin="anonymous"> -->
+
+	<!-- CoreUI CSS v3.4.0 -->
+	<link rel="stylesheet" href="https://unpkg.com/@coreui/coreui@3.4.0/dist/css/coreui.min.css" crossorigin="anonymous">
 
 	<title>@yield('title') | Artconnect</title>
 </head>
-<body class="c-app container-fluid">
-	<div class="c-wrapper">
-		<div class="c-body">
-			<main class="c-main">
-				@yield('content')
-			</main>
-		</div>
-	</div>
-	<script src="https://unpkg.com/@popperjs/core@2"></script>
-	<script src="https://unpkg.com/@coreui/coreui/dist/js/coreui.min.js"></script>
+<body class="c-app d-flex align-items-center">
+	@yield('content')
+	<script src="https://unpkg.com/@coreui/coreui@3.4.0/dist/js/coreui.bundle.min.js"></script>
 </body>
 </html>
